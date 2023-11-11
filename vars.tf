6910714c5adb7fcb55b14a8fc45f25a9b03436df
Nicholaus Suprapto <nsuprapt@uwaterloo.ca>
2023-11-10 22:47:25 -0500

    ses and s3 integration

--- vars.tf
+++ vars.tf
@@ -1,1 +1,7 @@
 variable profile_name { type = string }
+variable aws_region { type = string }
+variable aws_account_id { type = string }
+
+variable cloudflare_api_token { type = string }
+variable cloudflare_zone_id { type = string }
+
