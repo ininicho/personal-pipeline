3b9a2a1475ab0ab5ab64fbab8cdc2f58e0a6ebbb
Nicholaus Suprapto <nsuprapt@uwaterloo.ca>
2023-12-05 16:00:15 -0500

    lambda expense processor wip

--- lambda.tf
+++ lambda.tf
@@ -68,20 +68,40 @@ resource "aws_iam_role_policy_attachment" "email_lambda_logging_attachment" {
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
 
+resource "aws_iam_role" "processor_lambda_role" {
+  name               = "processor_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "processor_lambda_role_attachment" {
+  role       = aws_iam_role.processor_lambda_role.name
+  policy_arn = aws_iam_policy.read_write_s3.arn
+}
+
+resource "aws_iam_role_policy_attachment" "processor_lambda_logging_attachment" {
+  role       = aws_iam_role.processor_lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
 # Cloudwatch Group
-resource "aws_cloudwatch_log_group" "email_processor" {
-  name              = "/aws/lambda/${aws_lambda_function.email_processor.function_name}"
+resource "aws_cloudwatch_log_group" "email" {
+  name              = "/aws/lambda/${aws_lambda_function.email.function_name}"
   retention_in_days = 7
 }
 
-# Lambda Payload
+resource "aws_cloudwatch_log_group" "processor" {
+  name             = "/aws/lambda/${aws_lambda_function.processor.function_name}"
+  retention_in_days = 7
+}
+
+# Lambda
 data "archive_file" "email_lambda" {
   type        = "zip"
   source_dir  = "email_lambda"
   output_path = "email_lamba_payload.zip"
 }
 
-resource "aws_lambda_function" "email_processor" {
+resource "aws_lambda_function" "email" {
   filename      = data.archive_file.email_lambda.output_path
   function_name = "email_processor"
   role          = aws_iam_role.email_lambda_role.arn
@@ -98,11 +118,28 @@ resource "aws_lambda_function" "email_processor" {
   }
 }
 
+data "archive_file" "processor_lambda" {
+  type        = "zip"
+  source_dir  = "processor_lambda"
+  output_path = "processor_lambda_payload.zip"
+}
+
+resource "aws_lambda_function" "processor" {
+  filename      = data.archive_file.processor_lambda.output_path
+  function_name = "expense_processor"
+  role          = aws_iam_role.processor_lambda_role.arn
+  handler       = local.lambda_handler
+
+  source_code_hash = data.archive_file.processor_lambda.output_base64sha256
+
+  runtime = "python3.10"
+}
+
 # S3 Trigger
 resource "aws_lambda_permission" "allow_bucket_email" {
-  statement_id  = "AllowExecutionFromS3Bucket"
+  statement_id  = "AllowExecutionFromS3Bucket-Inbox"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.email_processor.function_name
+  function_name = aws_lambda_function.email.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.inbox.arn
 }
@@ -111,10 +148,29 @@ resource "aws_s3_bucket_notification" "bucket_notification_email" {
   bucket = aws_s3_bucket.inbox.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.email_processor.arn
+    lambda_function_arn = aws_lambda_function.email.arn
     events              = ["s3:ObjectCreated:*"]
   }
 
   depends_on = [aws_lambda_permission.allow_bucket_email]
+}
+
+resource "aws_lambda_permission" "allow_bucket_expense" {
+  statement_id  = "AllowExecutionFromS3Bucket-Expense"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.expense.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification_expense" {
+  bucket = aws_s3_bucket.expense.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket_expense]
 }
 
--- processor_lambda/lambda_function.py
+++ processor_lambda/lambda_function.py
@@ -1,10 +1,7 @@
-import os
 import urllib.parse
 import boto3
-from email import policy
-from email.parser import BytesParser
+import csv
 
-DEST_BUCKET = os.environ['DEST_BUCKET']
 s3 = boto3.client('s3')
 
 def handler(event, context):
@@ -12,32 +9,14 @@ def handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
-    # Get email from S3
+    # Get CSV from S3
     response = s3.get_object(Bucket=bucket, Key=key)
-    print('Email retrieved from S3')
-    raw_email = response['Body'].read()
+    print('CSV retrieved from S3')
+    raw_file = response['Body'].read()
 
-    email = BytesParser(policy=policy.default).parsebytes(raw_email)
+    # Read CSV file
+    reader = csv.DictReader(raw_file.decode('utf-8').splitlines())
+    print('CSV file read')
 
-    # Parse email attachment
-    for parts in email.walk():
-        if "attachment" == parts.get_content_disposition():
-            # Check file type, only accept csv
-            if parts.get_content_type() != 'text/csv':
-                print("Invalid file type: " + parts.get_content_type())
-                continue
-
-            attachment = parts.get_payload(decode=True)
-
-            # Get key from email
-            email_key = email['Bank']
-
-            # Save attachment in S3 and tag with email key
-            s3.put_object(Bucket=DEST_BUCKET, Key=f"{email_key}-{parts.get_filename()}", Body=attachment)
-            print('Attachment saved to S3')
-
-    # Delete email from S3
-    s3.delete_object(Bucket=bucket, Key=key)
-    print('Email deleted from S3')
-
-
+    # Print first row
+    print(next(reader))
